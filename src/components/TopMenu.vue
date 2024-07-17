e2cd39df195437aa7a4e5f7127af5851372b3889
TheJustRusik <7thejustrusik7@gmail.com>
2024-07-17 08:47:29 +0500

    New additions

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -2,34 +2,46 @@
 
   <header class="max-w-screen-lg mx-auto">
     <div class="flex justify-between items-center my-4">
-    <a href="/" class="transition ease-in-out hover:scale-110 duration-300">
-      <h1 class="logo text-red-500 text-5xl">kenuki</h1>
-      <h2 class="sublogo text-xs font-bold text-red-500 text-center">is TheJustRusik</h2>
-    </a>
+      <a href="/" class="transition ease-in-out hover:scale-110 duration-300">
+        <h1 class="logo text-red-500 text-5xl">kenuki</h1>
+        <h2 class="sublogo text-xs font-bold text-red-500 text-center">is TheJustRusik</h2>
+      </a>
+      <div class="flex items-center gap-6">
+        <v-btn-toggle
+          v-model="toggle"
+          variant="text"
+          color="red"
+          divided
+          mandatory
+        >
+          <v-btn class="transition ease-in-out hover:-translate-y-1 duration-300">Home</v-btn>
+          <v-btn class="transition ease-in-out hover:-translate-y-1 duration-300">Projects</v-btn>
+          <v-btn class="transition ease-in-out hover:-translate-y-1 duration-300">About</v-btn>
+        </v-btn-toggle>
 
-    <v-btn-toggle
-      v-model="toggle"
-      variant="text"
-      color="red"
-      divided
-    >
-      <v-btn class="transition ease-in-out hover:-translate-y-1 duration-300">Products</v-btn>
-      <v-btn class="transition ease-in-out hover:-translate-y-1 duration-300">Projects</v-btn>
-      <v-btn class="transition ease-in-out hover:-translate-y-1 duration-300">Comments</v-btn>
-      <v-btn class="transition ease-in-out hover:-translate-y-1 duration-300">About</v-btn>
-    </v-btn-toggle>
-
+        <v-btn :icon="darkTheme ? 'mdi-white-balance-sunny' : 'mdi-weather-night'" @click="toggleTheme" ></v-btn>
+      </div>
     
   </div>
-  <v-divider class="border-opacity-100 mb-4"></v-divider>
+  <v-divider class=" mb-4"></v-divider>
   </header>
   
 </template>
 
 <script setup>
+import { useTheme } from 'vuetify'
 import { ref } from "vue"
 
-var toggle = ref()
+
+const theme = useTheme()
+
+function toggleTheme () {
+  darkTheme.value = !darkTheme.value;
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+
+var toggle = ref(0)
+var darkTheme = ref(false)//true = dark, false = light
 
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <template>
+  
+  <header class="max-w-screen-lg mx-auto mb-6">
+    <h2 class="tracking-widest text-3xl mt-4 ">Hello👋</h2>
+    <h3 class="text-2xl font-light tracking-widest">My name is Ibraikhan Ruslan aka TheJustRusik, i'm Software Developer💻</h3>
+    <h3 class="text-2xl font-light tracking-widest">Hope, you will like my projects❤️</h3>
+
+  </header>
+
   <main class="max-w-screen-lg mx-auto">
     <div class="flex justify-between">
-      <h2 class="tracking-widest uppercase text-3xl mb-4">Products</h2>
+      <h2 class="tracking-widest uppercase text-3xl mb-4">Projects</h2>
       <v-btn append-icon="mdi-arrow-right" variant="outlined" color="blue">
-        See all products
+        See all projects
       </v-btn>
     </div>
     
@@ -13,19 +21,10 @@
       <ProductPreviewCard title="Hi" subtitle="Sub" text="Some text lol kek text lol kek text lol kek text lol kek text lol kek" image="https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg"></ProductPreviewCard>
       <ProductPreviewCard title="Hi" subtitle="Sub" text="Some text lol kek text lol kek text lol kek text lol kek text lol kek" image="https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg"></ProductPreviewCard>
     </div>
-
-    <div class="flex justify-between">
-      <h2 class="tracking-widest uppercase text-3xl mb-4">Projects</h2>
-      <v-btn append-icon="mdi-arrow-right" variant="outlined" color="blue">
-        See all projects
-      </v-btn>
-    </div>
-
   </main>
   
 </template>
 
 <script setup>
-  import { ref } from "vue";
   import ProductPreviewCard from '../components/ProductPreviewCard.vue'
 </script>
